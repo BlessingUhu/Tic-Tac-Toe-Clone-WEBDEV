cd9d182d91c665659262ae283419af07955b3190
BlessingUhu <Blessing2002.feb@gmail.com>
2023-09-30 15:49:34 -0400

    Working on scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,31 +1,55 @@
 const boardGame = document.getElementById("board");
 const gameButton = document.getElementById("playButton");
+const infoDisplay = document.getElementById("infoDisplay");
+
 const cellsInEachBoard = ["", "", "", "", "", "", "", "", ""];
 
-cellsInEachBoard.forEach((cell, index) => {
-  const box = document.createElement("div");
-  box.id = index;
-  box.className = "box";
-  boardGame.append(box);
-});
+let nameOfSymbolClass = "circle";
 
 function startGame() {
   gameButton.disabled = true;
-  const boxes = document.getElementsByClassName("box");
-  for (let box = 0; box < boxes.length; box++) {
-    //CIRCLE
-    const circle = document.createElement("div");
-    //CROSS
-    const cross = document.createElement("div");
-    cross.className = "cross"
-    cross.textContent = "X"
-
-    //EVENT LISTENER
-    boxes[box].addEventListener("click", (e) => {
-      circle.className = "circle";
-      boxes[box].append(circle);
-
-      console.log(boxes[box]);
-    });
-  }
+  cellsInEachBoard.forEach((cell, index) => {
+    const box = document.createElement("div");
+    box.id = index;
+    box.className = "box";
+    boardGame.append(box);
+    box.addEventListener("click", almostDone);
+  });
 }
+
+function almostDone(e) {
+  //CIRCLE OR CROSS
+  let ticTacToeSymbols = document.createElement("div");
+
+  displayTurn = "It is " + nameOfSymbolClass + "'s turn";
+  infoDisplay.textContent = displayTurn;
+  nameOfSymbolClass = nameOfSymbolClass === "circle" ? "cross" : "circle";
+
+  ticTacToeSymbols.className = nameOfSymbolClass;
+  e.target.append(ticTacToeSymbols);
+  e.target.removeEventListener("click", almostDone);
+  score();
+}
+
+function score() {
+  const boxes = document.getElementsByClassName("box");
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  winningCombos.forEach(combo=> 
+    combo.every(num =>{
+       
+    }
+        )
+    )
+}
+
+// infoDisplay.textContent("Circle wins!");
+// infoDisplay.style.backgroundColor = "green";
